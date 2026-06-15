883856be79f8a236bcf25199a3de03106f91b338
Konrad Michalik <km@move-elevator.de>
2026-06-15 16:44:09 +0200

    chore: add packaging metadata and security policy

---
+++ packaging_exclude.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "typo3_request_profiler" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'directories' => [
+        '.build',
+        '.ddev',
+        '.git',
+        '.github',
+        'build',
+        'node_modules',
+        'public',
+        'tests',
+        'var',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'CODE_OF_CONDUCT.md',
+        'CODEOWNERS',
+        'composer.lock',
+        'composer-require-checker.json',
+        'CONTRIBUTING.md',
+        '.editorconfig',
+        'editorconfig',
+        '.env',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        '.php-cs-fixer.cache',
+        '.php-cs-fixer.php',
+        'phpstan-baseline.neon',
+        'phpstan.neon',
+        'phpunit.xml',
+        'rector.php',
+        'renovate.json',
+        'SECURITY.md',
+        'version-bumper.yaml',
+    ],
+];
