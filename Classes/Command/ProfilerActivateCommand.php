cdaeb57e32e41393a00e1c0deb7938d9c3a37b35
Konrad Michalik <km@move-elevator.de>
2026-07-28 08:47:36 +0200

    feat: add profiler:activate and profiler:deactivate CLI commands

---
+++ Classes/Command/ProfilerActivateCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "typo3_request_profiler" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KonradMichalik\Typo3RequestProfiler\Command;
+
+use InvalidArgumentException;
+use KonradMichalik\Typo3RequestProfiler\Activation\{Duration, ProfilerStateService};
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{InputInterface, InputOption};
+use Symfony\Component\Console\Output\OutputInterface;
+
+use function is_string;
+use function sprintf;
+
+/**
+ * ProfilerActivateCommand.
+ *
+ * @author Konrad Michalik <hej@konradmichalik.dev>
+ */
+final class ProfilerActivateCommand extends Command
+{
+    public function __construct(
+        private readonly ProfilerStateService $stateService,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Temporarily activate request profiling (xdebug-style toggle).');
+        $this->addOption(
+            'duration',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'How long to keep profiling active, e.g. "15m", "1h", "300s" or a plain second count.',
+            '15m',
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $durationOption = $input->getOption('duration');
+        if (!is_string($durationOption)) {
+            $output->writeln('<error>Invalid duration: expected a string value.</error>');
+
+            return Command::FAILURE;
+        }
+
+        try {
+            $duration = Duration::fromString($durationOption);
+        } catch (InvalidArgumentException $exception) {
+            $output->writeln('<error>'.$exception->getMessage().'</error>');
+
+            return Command::FAILURE;
+        }
+
+        $expiresAt = $this->stateService->activate($duration);
+
+        $output->writeln(sprintf(
+            '<info>Profiling activated until %s (%d seconds).</info>',
+            date('c', $expiresAt),
+            $duration->seconds(),
+        ));
+
+        return Command::SUCCESS;
+    }
+}
---
+++ Classes/Command/ProfilerDeactivateCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "typo3_request_profiler" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KonradMichalik\Typo3RequestProfiler\Command;
+
+use KonradMichalik\Typo3RequestProfiler\Activation\ProfilerStateService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * ProfilerDeactivateCommand.
+ *
+ * @author Konrad Michalik <hej@konradmichalik.dev>
+ */
+final class ProfilerDeactivateCommand extends Command
+{
+    public function __construct(
+        private readonly ProfilerStateService $stateService,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Deactivate the temporary request profiling toggle.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->stateService->deactivate();
+
+        $output->writeln('<info>Profiling deactivated.</info>');
+
+        return Command::SUCCESS;
+    }
+}
